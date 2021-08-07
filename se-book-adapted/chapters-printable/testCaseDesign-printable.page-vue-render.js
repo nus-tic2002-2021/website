72f17b9aedc55c1d961416cb543a694083edb922
damithc <damithc@github>
2021-08-07 11:48:08 +0800

    Site Update. [skip ci]

--- se-book-adapted/chapters-printable/testCaseDesign-printable.page-vue-render.js
+++ se-book-adapted/chapters-printable/testCaseDesign-printable.page-vue-render.js
@@ -137,6 +137,6 @@ with(this){return _c('div',[_c('h2',{attrs:{"id":"recap"}},[_c('span',{staticCla
 with(this){return _c('div',{staticClass:"indented-less"},[_c('div')])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.6")]),_v(", generated on Sat, 7 Aug 2021, 1:29:46 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.6")]),_v(", generated on Sat, 7 Aug 2021, 11:45:29 GMT+8]")])])])}
 }];
   
